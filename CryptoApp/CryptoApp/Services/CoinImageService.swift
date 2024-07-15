3b905bfc13f43569d0eb42acfd8a9182222274c1
karam28 <ksbutter@gmail.com>
2024-07-15 18:46:48 +0530

    - Save coin icon in Local file manager

--- CryptoApp/CryptoApp/Services/CoinImageService.swift
+++ CryptoApp/CryptoApp/Services/CoinImageService.swift
@@ -13,26 +13,43 @@ class CoinImageService {
     
     @Published var image: UIImage? = nil
     private var imageSubscription: AnyCancellable?
-    
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
+    
     
     init(coin: CoinModel) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
 
+    
     private func getCoinImage() {
+        if let savedImage =  fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+            print("Retrieved image from File Manager")
+        } else {
+            downloadCoinImage()
+            print("Downloading image now")
+        }
+    }
+    
+    private func downloadCoinImage() {
+        guard let url = URL(string: coin.image) else { return }
         
-        guard let url = URL(string:  coin.image) else { return }
-                 
         imageSubscription = NetworkingManager.download(url: url)
-            .tryMap({ (data) -> UIImage? in
+            .tryMap { data -> UIImage? in
                 return UIImage(data: data)
+            }
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedImage in
+                guard let self = self, let image = returnedImage else { return }
+                self.image = image
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: image, imageName: self.imageName, folderName: self.folderName)
             })
-            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
-            })
-     }
+    }
+
     
 }
