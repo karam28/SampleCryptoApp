aad7106bd2b05f1fc62ba1772340e54188bf0775
karam28 <ksbutter@gmail.com>
2024-07-21 22:45:21 +0530

    - Search Bar filter using Combine

--- CryptoApp/CryptoApp/Core/Home/ViewModel/HomeViewModel.swift
+++ CryptoApp/CryptoApp/Core/Home/ViewModel/HomeViewModel.swift
@@ -32,5 +32,30 @@ class HomeViewModel: ObservableObject {
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+        
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map (filterCoins)
+            .sink { [weak self] returnedCoin in
+                self?.allCoins = returnedCoin
+            }
+            .store(in: &cancellables)
+    }
+    
+    
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        
+        guard !text.isEmpty else {
+            return coins
+        }
+        let lowercaseText = text.lowercased()
+        
+        return coins.filter { coin -> Bool in
+            return coin.name.lowercased().contains(lowercaseText) ||
+            coin.symbol.lowercased().contains(lowercaseText) ||
+            coin.id.lowercased().contains(lowercaseText)
+        }
+        
     }
 }
